68c852eede114204024d106f7ca6d882a9c27a3f
Junya Kono <junya03dance@gmail.com>
2023-07-31 00:26:15 +0900

    update

--- app/revalidate/page.tsx
+++ app/revalidate/page.tsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-anonymous-default-export, react/display-name
 export default async function () {
   await fetch("https://httpbin.org/status/200", {
-    next: { revalidate: false },
+    next: { revalidate: 60 * 60 * 24 },
   });
 
   return <h1>revalidate=false</h1>;
